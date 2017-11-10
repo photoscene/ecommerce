8402663d8bf5607eaf5ede59c78f8f0252523c1b
Damien Bouvy <dbo@odoo.com>
2017-11-10 13:38:09 +0100

    [ADD] module crash_test

---
+++ crash_module/__manifest__.py
@@ -0,0 +1,13 @@
+
+{
+    'name': 'Crash App',
+    'version': '1.0',
+    'category': 'Bad',
+    'sequence': 6,
+    'summary': 'Crash the tests on purpose',
+    'depends': ['base'],
+    'data': [
+    	'security/crash.xml',
+    ],
+    'application': True,
+}
