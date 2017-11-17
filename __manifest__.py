ecc2926a9a47340e476ab892fcc8c0c19ef74d26
Csaba Fazekas <csabafazekasster@gmail.com>
2017-11-17 13:51:40 +0100

    [ADD] adding new modules

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Crash App',
+    'version': '1.0',
+    'category': 'Bad',
+    'sequence': 6,
+    'summary': 'Crash the tests on purpose',
+    'depends': ['base'],
+    'data': [
+        'security/crash.xml',
+    ],
+    'application': True,
+}
